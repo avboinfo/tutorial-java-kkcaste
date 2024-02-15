ac84d9f9debf92b5f939248a257379812387813c
kkcaste <150423975+kkcaste@users.noreply.github.com>
2024-02-15 12:19:53 +0000

    a

---
+++ F_File/ProveFile.java
@@ -0,0 +1,21 @@
+import java.io.File;  
+import java.io.PrintWriter;  
+import java.lang.Math;
+
+public class ProveFile{
+    public static void main(String[] args){
+
+    File f=new File("random.csv");
+    try{
+        PrintWriter scrittore=new PrintWriter( f);
+        for(int i=0;i<10;i++){
+            scrittore.printf("%05.2f\t%05.2f\n, Math.random()*100 , Math.random()*100");
+
+        }   
+        scrittore.close();
+        
+     }catch(Exception e){
+        e.printStackTrace();
+     }
+}
+}
