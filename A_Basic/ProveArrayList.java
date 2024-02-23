4e48d4ed9b463f2e5439c69b3fc3fe4a9b3300a9
kkcaste <150423975+kkcaste@users.noreply.github.com>
2024-02-23 09:16:06 +0000

    arraylist prove

---
+++ A_Basic/ProveArrayList.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ProveArrayList{
+
+    public static void main (String[] args){
+        System.out.println("Program started");
+
+        ArrayList<String> a=new ArrayList<>();
+        a.add("Uno");
+        a.add("Due");
+        a.add("Tre");
+        a.add("Quattro");
+        a.add("Cinque");
+        a.add("Sei");
+        a.add(1,"Uno virgola cinque");
+
+        //Collections.sort(a);  
+        for(String i : a){
+            System.out.println(i);
+        }
+
+        System.out.println(a);
+
+        //System.out.println("j".compareTo("p"));
+
+
+        
+    }
+
+}
